edadc6c31cbaebc86fb66d8abe7b6a4108f474f8
John Carl Quieta <quieta.johncarl158@gmail.com>
2025-04-08 13:44:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import requests
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -25,4 +26,5 @@ def get_branches():
     return jsonify(res.json())
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
